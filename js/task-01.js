8ead052e1aa234805f7e76a3ec4862a4837ff86b
olita16 <119816415+olita16@users.noreply.github.com>
2023-12-17 21:07:25 +0100

    correct 1st task

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,14 @@
-const categoriesList = document.getElementById('categories');
-const items = categoriesList.querySelectorAll('li.item');
+
+const items = categories.querySelectorAll('.item');
 
 console.log(`Number of categories: ${items.length}`);
+console.log('\n');
 
-items.forEach((item) => {
-  const title = item.querySelector('h2').textContent;
-  const elements = item.querySelectorAll('ul li');
+items.forEach(item => {
+  const title = item.firstElementChild;
+  const elements = item.lastElementChild.children;
 
-  console.log(`Category: ${title}`);
+  console.log(`Category: ${title.textContent}`);
   console.log(`Elements: ${elements.length}`);
+  console.log('\n');
 });
-
-
